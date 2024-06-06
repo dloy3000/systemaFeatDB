22cb7c2137747c39905a33ae6cbea0c53e9253d9
Devon A Loy <dloy3000@gmail.com>
2024-06-05 22:12:39 -0400

    Added basic searchbox.

---
+++ src/interface/tiles/search/SearchBox.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { FormEvent, useEffect, useState } from "react"
+
+export function SearchBox() {
+    const [search, openSearch] = useState(false);
+    const [fields, useFields] = useState("");
+
+    return (
+        <div className="cardGrid">
+            {search ? <input className="searchBox" type='search' id='keywordSearch' name='search-bar'
+                onInput={(event: FormEvent<HTMLInputElement>) => {
+                    //@ts-ignore
+                    const value = event.target.value; //event.target.value shows a non-existence error for some reason even though it does exist on callback.
+                    useFields(typeof value === 'string' ? value : "");
+                }} /> : <></>}
+            <button className={search ? "searchButton open" : "searchButton closed"} onClick={() => {
+                openSearch(!search);
+            }}></button>
+        </div>
+    )
+}
